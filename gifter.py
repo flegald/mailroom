699a40316ca3c6497966184c561fcf310c36c211
David Flegal <flegal.david@gmail.com>
2016-03-06 15:28:52 -0800

    adding tests

--- gifter.py
+++ gifter.py
@@ -1,7 +1,7 @@
 # -*- coding:UTF-8 -*-
 
 import sys
-gifter_dict = {}
+GIFTER_DICT = {}
 
 email_text = "Dear {}, \n Thank you so much for your donation of ${}. You are one cool cat \n"
 report_text = "Name: {} Total Donatations: {} Number of Times Donated: {}"
@@ -22,14 +22,14 @@ def send_or_report():
     if validator(user_answer):
         ask_name()
     elif validator(user_answer) == False:
-        create_report(gifter_dict)
+        create_report(GIFTER_DICT)
 
     
 
 def ask_name():
     name_givenT = raw_input("Type gifter name or 'list' to see a list of gifters. \n >>>")
     if name_givenT.lower() == "list":
-        print list(gifter_dict)
+        print list(GIFTER_DICT)
         ask_name()
     elif name_givenT.lower() == 'q':
         sys.exit()
@@ -48,20 +48,20 @@ def ask_ammt(name):
         update_values(update_name, int(ammt_givenT))
 
 def check_dic(name):
-    if name in gifter_dict:
+    if name in GIFTER_DICT:
         ask_ammt(name)
     else:
-        gifter_dict.update({name: {'name': name, 'total_ammt': 0, 'this_ammt': 0, 'times_given': 0 }})
+        GIFTER_DICT.update({name: {'name': name, 'total_ammt': 0, 'this_ammt': 0, 'times_given': 0 }})
         ask_ammt(name)
 
 def update_values(name, ammt):
-    temp_key = gifter_dict.get(name)
+    temp_key = GIFTER_DICT.get(name)
     get_total = temp_key.get('total_ammt')
     total = (int(get_total) + ammt)
-    temp_total_given = gifter_dict.get(name)
+    temp_total_given = GIFTER_DICT.get(name)
     get_total_times = temp_total_given.get('times_given')
     total_times = (1 + get_total_times)
-    gifter_dict.update({name: {'name': name, 'total_ammt': total, 'this_ammt': ammt, 'times_given': total_times}})
+    GIFTER_DICT.update({name: {'name': name, 'total_ammt': total, 'this_ammt': ammt, 'times_given': total_times}})
     print_email(name, ammt)
 
 def print_email(name, ammt):
@@ -70,10 +70,10 @@ def print_email(name, ammt):
 
 def create_report(dict):
     gifter_list = []
-    for idx, gifter in enumerate(gifter_dict):
-        name = gifter_dict[gifter].get("name")
-        total = gifter_dict[gifter].get("total_ammt")
-        num_of_times = gifter_dict[gifter].get("times_given")
+    for idx, gifter in enumerate(GIFTER_DICT):
+        name = GIFTER_DICT[gifter].get("name")
+        total = GIFTER_DICT[gifter].get("total_ammt")
+        num_of_times = GIFTER_DICT[gifter].get("times_given")
         avg_donation = int(total)/int(num_of_times)
         gifter_list.append("Total:${} Name: {} Number of Times: {} Average:${}".format(int(total), name.upper(),  num_of_times, avg_donation))
     organize_list(gifter_list)
