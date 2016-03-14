8dc001409dee2250c4c31d6d48a7ebd13feab087
David Flegal <flegal.david@gmail.com>
2016-03-14 04:32:28 -0700

    total redo

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
+"""Setup file for Mail Room."""
 
 from setuptools import setup
 
 setup(
-  name="gifter",
+  name="Donate Tracker",
   description="quickly keep track of gifters and their donations",
   version=0.1,
   author="David Flegal",
   author_email="flegal.david@gmail.com",
   license="MIT",
-  py_modules=["gifter"],
+  py_modules=["mail_room"],
   package_dir={'': 'src'},
-  install_requires=['future', 'builtins'],
+  install_requires=['sys'],
   extras_require={'test': ['pytest', 'pytest-xdist', "tox"]},
   )
